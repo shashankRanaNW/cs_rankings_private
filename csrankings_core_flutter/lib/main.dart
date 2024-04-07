8983cf81095b2619a03a1275629d43b7f59009e8
AdiKul1294 <f20211294@goa.bits-pilani.ac.in>
2024-04-07 09:07:29 +0530

    changed name

--- csrankings_core_flutter/lib/main.dart
+++ csrankings_core_flutter/lib/main.dart
@@ -350,7 +350,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'CORE CSRankings',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           // This is the theme of your application.
